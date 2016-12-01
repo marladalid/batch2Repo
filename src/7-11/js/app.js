2f8e747b4cca283ef93d58df2f3800443ef5c206
dalid <dalidzmarl8@gmail.com>
2016-12-01 21:11:33 +0800

    Displayed JSON information using JQuery Javascript

--- src/7-11/js/app.js
+++ src/7-11/js/app.js
@@ -1,20 +1,20 @@
 /**
  * Created by JPMPC-B208 on 11/24/2016.
  */
-var n=5;
+
+/*var n=5;
 var x=1;
 var result = n+x;
 
-$("#btn1").click(function () {
-    $("div").css({background: "red"});
+/!*$("#btn1").click(function () {
+
     var divCount = $("div").length;
     for(var i=0;i<divCount;i++){
 
     }
-
-    alert("There are = " + i + " divs and it will change the background colors into RED");
-
-});
+    // alert("There are = " + i + " divs and it will change the background colors into RED");
+    $("div").css({background: colors[random]});
+});*!/
 
 
 document.getElementById("result").innerHTML = "result is " + result;
@@ -25,7 +25,7 @@ mySubmitBtn.onclick = function (event) {
     mySubmitBtn.style.display = "none";
 };
 
-/* START OF AUDIO */
+/!* START OF AUDIO *!/
 
 var playPauseBtn = document.getElementById("playBtn");
 var audio = document.getElementById("myAudio");
@@ -40,6 +40,29 @@ var audio = document.getElementById("myAudio");
             playPauseBtn.textContent = "PLAY";
         }
     };
+    $("#btn1").click( function () {
+        findDivs();
+    })
+
+
+    /!*setInterval(function () {
+        findDivs();
+    }, 1000);*!/
+
+    function findDivs() {
+            var divCount = $("div");
+            var colors = Array("yellow" , "pink", "green", "black", "red");
+
+
+
+            for(var i=0;i<divCount.length;i++){
+                var random = Math.floor(Math.random() * colors.length) ;
+                // alert("There are = " + i + " divs and it will change the background colors into RED");
+                // divCount[randomDiv].style.backgroundColor = colors[random];
+                divCount.eq(i).css({background: colors[random]});
+            }
+    }
+
 
     function stopAudio() {
         audio.pause();
@@ -47,9 +70,9 @@ var audio = document.getElementById("myAudio");
         playPauseBtn.textContent = "PLAY";
     }
 
-/* END OF AUDIO */
+/!* END OF AUDIO *!/
 
-/* START OF VIDEO*/
+/!* START OF VIDEO*!/
 
 var vidPlayPauseBtn = document.getElementById("vidPlayBtn")
 var video = document.getElementById("myVideo");
@@ -74,6 +97,85 @@ function vidStopAudio() {
 
 function fullScreen() {
     video.webkitRequestFullscreen();
-}
+}*/
 
 /* END OF VIDEO */
+
+/* START OF AJAX ACTIVITY */
+
+
+$("#demo-btn").click(function () {
+    $.ajax({
+        method: "GET",
+        url: "ajax_info.json",
+        error: function (a,b,c) {
+            console.log(c);
+        },
+        success: function (data,a,b,c) {
+            $("#demo").text(data);
+            //console.log(c);
+        }
+    });
+});
+
+$("#json-btn").click(function () {
+    $.ajax({
+        method: "GET",
+        url: "ajax_info.json",
+        dataType: "json",
+        error: function (a,b,c) {
+            console.log(b);
+        },
+        success: function (data,a,b,c) {
+            $.each(data.employees, function (i,user) {
+                $("#json-demo").append("<p>" + user.firstName + " " + user.lastName + "</p>");
+                /*content = "<p>" + user.firstName + " " + user.lastName + "</p>";
+                $(content).appendTo("#json-demo");*/
+            });
+            //$("#json-btn").text();
+            //console.log(c);
+        }
+    });
+});
+
+$(document).on("click",".actions a",function (event) {
+    event.preventDefault();
+});
+
+$(document).on("click","#removeDiv",function(event){
+    event.preventDefault();
+
+    var unique_id = $(this).attr("data-id");
+
+    $("#"+unique_id).fadeOut();
+});
+
+
+//$("#json-act-btn").click(function(){
+    $.ajax({
+        method:'GET',
+        url:'json/json_activity.json',
+        dataType:'json',
+        error:function(){
+            alert('error');
+        },
+        success:function(response){
+            $.each(response.data,function(a,field){
+                $("#results").append("<div id="+field.id+" class=\"res-container\">"+
+                                        "<span class=\"remove-btn medium-text\"><a data-id="+field.id+" id='removeDiv' href='#' title='Remove this Post'>X REMOVE</a></span>"+
+                                        "<div class=\"from\"><a href="+">"+field.from.name+"</a><div class='medium-text'>"+"Posted on: "+field.created_time+"</div></div>"+
+                                        "<div class=\"message\">"+field.message+"</div>"+
+                                        "<div class=\"actions\">"+
+                                            "<ul>" +
+                                                "<li><a href="+field.actions[1].link+">"+field.actions[1].name+"</li>"+
+                                                "<li><a href="+field.actions[0].link+">"+field.actions[0].name+"</li>"+
+                                            "</ul>"+
+                                        "</div>"+
+                                    "</div>"
+                );
+            });
+        }
+    });
+//});
+
+
